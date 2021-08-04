0d11fbcb3f07fa064e11dec26272685e58fe385b
adinahanna <adina.weinberg@gmail.com>
2021-08-04 14:08:45 -0500

    tab navigation working

--- App.js
+++ App.js
@@ -1,17 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Nav from './navigation/TabNavigator';
+import Nav from './navigation/Navigator';
 import CommentFeed from './screens/CommentFeed';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text></Text>
-      <StatusBar style="hidden" />
-      {/* <CommentFeed /> */}
+    <>
       <Nav />
-    </View>
+    </>
   );
 }
 
