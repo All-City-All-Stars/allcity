b3af6e52c7b21036e4c0ac35414343f2342a3fd8
yetifunk <charlesdemar87@gmail.com>
2021-08-04 08:58:47 -0600

    header component

---
+++ components/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Image, StyleSheet, View } from 'react-native';
+
+export default function Header() {
+    return (
+        <View style={style.header}>
+            <Image style={styles.logo} source={require('../assets/AllCityLogo.png')} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    header: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'gold',
+        borderBottomColor: "black",
+        borderBottomWidth: 1,
+    },
+    logo: {
+        width: 300,
+        height: 80,
+        marginTop: 20,
+        shadowColor: "#FF1493",
+        shadowOffset: {
+            width: 0,
+            height: 8,
+        },
+        shadowOpacity: 0.85,
+        shadowRadius: 5.00,
+        elevation: 24,
+    },
+})
