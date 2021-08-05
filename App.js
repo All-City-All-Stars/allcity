1adf9636a35d49922744e4137e54405e0b91850e
CrowdedAstronaut <ross@johnrossphillips.com>
2021-08-05 10:32:15 -0500

    New Post Form is Linked

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Nav from './navigation/Navigator';
-import Header from './components/Header';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Nav from "./navigation/Navigator";
+import Header from "./components/Header";
+import NewPostForm from "./screens/NewPostForm";
 
 export default function App() {
-
   return (
     <>
-        <Header />
-        <Nav />
+      <Header />
+      {/* <NewPostForm /> */}
+      <Nav />
     </>
   );
 }
@@ -17,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
